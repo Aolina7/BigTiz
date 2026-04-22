ad544729c8dfa00f7e8517b48f89f38782eb0294
Olina <alina.usupova2007@gmail.com>
2026-04-23 03:46:51 +0500

    adding header, ruble, hamburger

--- app/src/main/java/com/example/bigtiz/MainActivity.kt
+++ app/src/main/java/com/example/bigtiz/MainActivity.kt
@@ -4,20 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bigtiz.ui.theme.BigTizTheme
 
+var money by mutableStateOf(10000)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BigTizTheme {
                 DrawSurface()
+                UpperOval().DrawUpperOval()
             }
         }
     }
@@ -34,5 +60,95 @@ fun DrawSurface() {
             contentDescription = "", //описание фона
             contentScale = androidx.compose.ui.layout.ContentScale.Crop //Crop увеличивает изображение, чтобы полность покрывало экран
         )
+    }
+}
+
+//шапка экрана
+class UpperOval {
+    @Composable
+    fun DrawUpperOval() : Unit {
+        val list = listOf(Color.Gray, Color.Gray, Color.White)
+        Box(
+            modifier = Modifier
+                .padding(vertical = 25.dp)
+                .padding(start = 5.dp)
+                .padding(end = 5.dp)
+                .fillMaxWidth()
+                .height(60.dp)
+                .clip(RoundedCornerShape(50))
+                .background(Color.Gray.copy(alpha = 0.5f))
+                .padding(horizontal = 20.dp),
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = androidx.compose.ui.Alignment.Center,
+            ){
+                Text(
+                    text = "Бик Тиз",
+                    fontSize = 25.sp,
+                    style = TextStyle(
+                        brush = Brush.linearGradient(list)
+                    )
+
+                )
+            }
+            DrawHamburgerMenu()
+
+        }
+    }
+
+    //контекстное меню
+    @Composable
+    fun DrawHamburgerMenu() : Unit {
+        Box(
+            modifier = Modifier.size(30.dp, 56.dp)
+        ) {
+            IconButton(
+                onClick = {print("Тут окно 1")}, // ....
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Icon(Icons.Default.Menu,
+                    contentDescription = "Показать меню",
+                    modifier = Modifier.fillMaxSize(),
+                    tint = Color.LightGray)
+            }
+        }
+        MoneyIcon()
+    }
+
+    //деньги
+    @Composable
+    fun MoneyIcon() : Unit {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 60.dp)
+                .padding(vertical = 20.dp),
+            contentAlignment = androidx.compose.ui.Alignment.TopEnd
+        )
+        {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+                verticalAlignment = androidx.compose.ui.Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ruble),
+                    contentDescription = "Сумма на счету",
+                    modifier = Modifier.size(40.dp),
+                    contentScale = androidx.compose.ui.layout.ContentScale.Crop
+
+                )
+                Text(
+                    text = "$money",
+                    modifier = Modifier
+                        .height(30.dp)
+                        .padding(vertical = 2.dp),
+                    fontWeight = FontWeight.Bold
+                )
+
+
+            }
+        }
     }
 }
