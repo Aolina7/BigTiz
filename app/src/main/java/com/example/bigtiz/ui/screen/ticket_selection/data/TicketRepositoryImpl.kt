fa165be0a3c0b89d0df8ff3d0db26f111883022a
Sanechkaprog <165583946+Sanechkaprog@users.noreply.github.com>
2026-05-07 19:39:34 +0300

    clean architecture 2nd attempt

---
+++ app/src/main/java/com/example/bigtiz/ui/screen/ticket_selection/data/TicketRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.example.bigtiz.ui.screen.ticket_selection.data
+
+import com.example.bigtiz.ui.screen.ticket_selection.domain.TicketRepository
+import com.example.bigtiz.ui.screen.ticket_selection.domain.Tickets
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+
+class TicketRepositoryImpl(
+    private val file: File
+) : TicketRepository {
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+    }
+
+    override fun getTickets(): Tickets {
+        val text = file.readText()
+        return json.decodeFromString(text)
+    }
+
+    override fun saveTickets(tickets: Tickets) {
+        file.writeText(json.encodeToString(tickets))
+    }
+}
---
+++ app/src/main/java/com/example/bigtiz/ui/screen/ticket_selection/domain/TicketPrices.kt
@@ -0,0 +1,7 @@
+package com.example.bigtiz.ui.screen.ticket_selection.domain
+
+object TicketPrices {
+    const val FAN = 100
+    const val VIP = 1400
+    const val PREMIUM = 2500
+}
