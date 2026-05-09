946b353b22c8d3d4a8c05ee0d82882b9e102b58f
anita <anitatalbieva@gmail.com>
2026-05-09 14:24:42 +0300

    реализован класс ViewModelFactory для создания ViewModel с параметрами

--- app/src/main/java/com/example/bigtiz/ui/screen/pilot_details/presentation/PilotDetailsViewModelFactory.kt
+++ app/src/main/java/com/example/bigtiz/ui/screen/pilot_details/presentation/PilotDetailsViewModelFactory.kt
@@ -1,4 +1,29 @@
 package com.example.bigtiz.ui.screen.pilot_details.presentation
 
-class PilotDetailsViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.bigtiz.data.datasource.LocalRacerDataSource
+import com.example.bigtiz.data.repository.RacerRepositoryImpl
+import com.example.bigtiz.domain.usecase.GetAllRacersUseCase
+import com.example.bigtiz.domain.usecase.GetRacerByIdUseCase
+
+class PilotDetailsViewModelFactory(
+    private val racerId: Int
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PilotDetailsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return PilotDetailsViewModel(
+                getAllRacersUseCase = GetAllRacersUseCase(
+                    RacerRepositoryImpl(LocalRacerDataSource())
+                ),
+                getRacerByIdUseCase = GetRacerByIdUseCase(
+                    RacerRepositoryImpl(LocalRacerDataSource())
+                ),
+                initialRacerId = racerId
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
