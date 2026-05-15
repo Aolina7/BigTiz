a437f985219b2b06f2868b907d8f09f8cd9854aa
anita <anitatalbieva@gmail.com>
2026-05-15 13:27:36 +0300

    написан класс welcomeContent в domain

---
+++ app/src/main/java/com/example/bigtiz/ui/screen/welcome/domain/WelcomeContent.kt
@@ -0,0 +1,9 @@
+package com.example.bigtiz.ui.screen.welcome.domain
+
+data class WelcomeContent(
+    val title: String = "BIGTIZ",
+    val subtitle: String = "F1 TEAM FROM TATARSTAN",
+    val welcomeMessage: String = "Добро пожаловать в мир\nскорости и адреналина",
+    val buttonText: String = "СТАРТ",
+    val buttonHint: String = "Нажмите, чтобы начать"
+)
